d604110dfe17b6dfc2fc1c8af91057fbe69bb811
Your Name <vaibhavlohani7251@gmail.com>
2026-07-08 23:28:08 +0530

    Final_Query_consecutive

--- SQL/ConsectiveNumbers.sql
+++ SQL/ConsectiveNumbers.sql
@@ -16,9 +16,7 @@
 -- # Find all numbers that appear at least three times consecutively.
 -- # Return the result table in any order.
 
-
-SELECT num as ConsecutiveNumbers FROM(
-    select num, LAG(num) OVER (ORDER BY id) AS prev_num,
-    LEAD(num) OVER(ORDER BY id) AS next_num
-    FROM Logs) t
-    WHERE prev_num = num and next_num = num;               )
+SELECT DISTINCT as ConsecutiveNums FROM (
+    SELECT num, LAG(num) OVER (ORDER BY id DESC) AS prev_num,
+    LEAD(num) OVER (ORDER BY id DESC) as next_num FROM Logs) t
+    WHERE prev_num = num and  next_num = num;              )
